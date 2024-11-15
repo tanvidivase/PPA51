f325218cb355c7327b9ecf6e378c86cc609441c0
tanvidivase <164602229+tanvidivase@users.noreply.github.com>
2024-11-15 20:58:36 +0530

    Add files via upload

---
+++ Addition2.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    int No1 = 0;
+    int No2 = 0;
+    int Ans = 0;
+   printf("enter First Number: \n");
+   scanf("%d",&No1);
+
+   printf("enter second number: \n");
+   scanf("%d",&No2);
+
+   Ans = No1 + No2;
+   printf("Addition is: %d\n", Ans);
+
+
+    return 0;
+}
